71164f3bd9239db36bb336448151253908229c17
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-12-31 19:53:12 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,29 @@
+// const express = require('express');
+// const app = express();
+// require('dotenv').config();
+// const port = process.env.PORT  ;
+// const cors = require('cors');
+// const userNameRoute = require('./Routes/UsernameRoute')
+// // const mainRoute = require('../Backend/Routes/MainRoute')
+// const captureSS = require('../Backend/Routes/Screenshot')
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use('/api',userNameRoute)
+// // app.use('/api',mainRoute)
+// app.use('/api',captureSS)
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+
 const express = require('express');
 const app = express();
 require('dotenv').config();
@@ -9,9 +35,13 @@ const captureSS = require('../Backend/Routes/Screenshot')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use('/api',userNameRoute)
-// app.use('/api',mainRoute)
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with the specific origin(s) you want to allow
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}, userNameRoute);
 app.use('/api',captureSS)
 
 
@@ -23,3 +53,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
